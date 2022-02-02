f8e0039718599d0d39a132e8821e0f42a5f2dffd
Tich <tich.tzvidzayi@pargo.co.za>
2022-02-02 12:39:54 +0200

    v1

---
+++ posty/resources/views/layouts/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+
+
+        <h1> App</h1>
+        <p> 111010100</p>
+
+    </head>
+    <body class="antialiased">
+
+    </body>
+</html>
---
+++ posty/resources/views/posts/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Posts</title>
+
+
+        <h1> Posts </h1>
+        <p> 00000000000.</p>
+
+    </head>
+    <body class="antialiased">
+
+    </body>
+</html>
