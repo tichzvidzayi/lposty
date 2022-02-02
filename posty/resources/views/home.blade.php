fd004e8adbdd99c1df97eb986d89c0d95b78a553
Tich <tich.tzvidzayi@pargo.co.za>
2022-02-02 12:27:03 +0200

    v1

---
+++ posty/resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Home</title>
+
+
+        <h1> Welcome Home </h1>
+        <p> There is no place better than home.</p>
+
+    </head>
+    <body class="antialiased">
+
+    </body>
+</html>
